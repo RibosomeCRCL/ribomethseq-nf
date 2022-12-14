c5da919aa3bedd1b5b0cf89895bb845cfb812390
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2022-12-14 16:58:12 +0100

    QC report improvements

--- Rscripts/QC/merge_counts.R
+++ Rscripts/QC/merge_counts.R
@@ -1,5 +1,7 @@
 merge_counts <- function(count_list, rna_col, position_col,count_col) {
-    
+   # Merge all count dataframes contained in count_list
+   # into a single dataframe "df_all".
+
   sample_list <- count_list
   sample_list_nm <- names(sample_list)
   
@@ -7,7 +9,7 @@ merge_counts <- function(count_list, rna_col, position_col,count_col) {
   # (the positions are identical to all samples, as they are aligned on same reference)
   position_list <- sample_list[[1]][,c(rna_col,position_col)]
   position_list <- paste0(position_list[,1],"_",position_list[,2])
-  matrix_all <- data.frame(position = position_list)
+  df_all <- data.frame(position = position_list)
 
     for(sample_nm in sample_list_nm) {
     sample_df <- sample_list[[sample_nm]]
@@ -16,11 +18,11 @@ merge_counts <- function(count_list, rna_col, position_col,count_col) {
     sample_df["position"] <- position_list
     count_col <- 3
     sample_df <- sample_df[,c(4,count_col)]
-    matrix_all <- dplyr::full_join(matrix_all,sample_df,by="position")
-    names(matrix_all)[length(names(matrix_all))] <- sample_nm 
-    matrix_all <- matrix_all[match(position_list,matrix_all[,"position"]),]
+    df_all <- dplyr::full_join(df_all,sample_df,by="position")
+    names(df_all)[length(names(df_all))] <- sample_nm 
+    df_all <- df_all[match(position_list,df_all[,"position"]),]
     }
 
-    return(matrix_all)
+    return(df_all)
   
   }
