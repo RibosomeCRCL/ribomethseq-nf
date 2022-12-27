42a802acad365b76e914cc576d3df0de4428081b
Anthony Ferrari <anthony.ferrari@lyon.unicancer.fr>
2022-12-27 18:36:36 +0100

    removed dependency to merge_counts

--- Rscripts/QC/merge_counts.R
+++ Rscripts/QC/merge_counts.R
@@ -1,10 +1,10 @@
-merge_counts <- function(count_list, rna_col, position_col,count_col) {
+merge_counts <- function(count_list, rna_col, position_col, count_col) {
    # Merge all count dataframes contained in count_list
    # into a single dataframe "df_all".
 
   sample_list <- count_list
   sample_list_nm <- names(sample_list)
-  
+
   # Get the positions from on sample and use them to prepare our merged dataframe
   # (the positions are identical to all samples, as they are aligned on same reference)
   position_list <- sample_list[[1]][,c(rna_col,position_col)]
@@ -19,10 +19,10 @@ merge_counts <- function(count_list, rna_col, position_col,count_col) {
     count_col <- 3
     sample_df <- sample_df[,c(4,count_col)]
     df_all <- dplyr::full_join(df_all,sample_df,by="position")
-    names(df_all)[length(names(df_all))] <- sample_nm 
+    names(df_all)[length(names(df_all))] <- sample_nm
     df_all <- df_all[match(position_list,df_all[,"position"]),]
     }
 
     return(df_all)
-  
+
   }
