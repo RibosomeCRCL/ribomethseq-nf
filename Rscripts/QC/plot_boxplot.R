0b246ca2a36fa1bd30d055fc708064aa1330d49f
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-01-09 15:29:33 +0100

    Replace reshape2 with tidyr

--- Rscripts/QC/plot_boxplot.R
+++ Rscripts/QC/plot_boxplot.R
@@ -1,16 +1,12 @@
-plot_boxplot_samples <- function(matrix,values_col_name,values_to_plot,show_outlier) {
-  id_vars <- "sample"
+plot_boxplot_samples <- function(matrix,show_outlier) {
   matrix["position"] <- NULL
 
-  matrix_inv <- as.data.frame(t(matrix))
-  matrix_inv <- tibble::rownames_to_column(matrix_inv,"sample")
 
-  matrix_melted <- reshape2::melt(matrix_inv, id.vars = id_vars, value.name = values_col_name)  
+  matrix_melted <- tidyr::gather(matrix, key = sample, value = counts)
   shape_outlier <- NA
   if(show_outlier) shape_outlier <- 19
-  
 
-  p <- ggplot2::ggplot(matrix_melted, aes_string(x = "sample", y = values_to_plot)) +
+  p <- ggplot2::ggplot(matrix_melted, aes_string(x = "sample", y = "log10(counts)")) +
       geom_boxplot(outlier.shape = shape_outlier) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
   p <-  p +  geom_hline(yintercept = 2, colour = "blue") 
